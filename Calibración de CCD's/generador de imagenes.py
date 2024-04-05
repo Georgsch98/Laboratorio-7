0c891d256df17a39fac970431942704ec34aeb93
Georg <124269910+Georgsch98@users.noreply.github.com>
2024-04-05 11:39:25 -0300

    Update generador de imagenes.py

--- Calibración de CCD's/generador de imagenes.py
+++ Calibración de CCD's/generador de imagenes.py
@@ -28,7 +28,14 @@ for archivo in os.listdir(directorio):
                 promedio_datos += (datos_imagen - promedio_datos) / total_imagenes
 
 # Calcular la varianza del conjunto de datos de todas las imágenes
-varianza_acumulada = np.sum((datos_imagen - promedio_datos) ** 2 for datos_imagen in datos_imagenes) / (total_imagenes-1)
+#varianza=[]
+##for i in datos_imagen:
+##    x=(i-promedio_datos)**2
+##    varianza.append(x)
+##print(varianza, "lista varianza")
+##varianza_datos=np.sum(varianza)/(total_imagenes-1)
+
+varianza_datos = np.sum((datos_imagen - promedio_datos) ** 2 for datos_imagen in datos_imagenes) / (total_imagenes-1)
 
 # Convertir los datos de la varianza a uint16
 #varianza_final = varianza_acumulada.astype(np.uint16)
@@ -40,6 +47,43 @@ varianza_acumulada = np.sum((datos_imagen - promedio_datos) ** 2 for datos_image
 #print(f"Se ha creado la imagen de varianza '{nombre_archivo_varianza}' con los datos de varianza del conjunto de {total_imagenes} imágenes.")
 
 print(promedio_datos.shape, "dimension del promedio")
-print(varianza_acumulada.shape, "dimension de la varianza")
+print(varianza_datos.shape, "dimension de la varianza")
 print(promedio_datos, "matriz promedio")
-print(varianza_acumulada, "matriz varianza")
+print(varianza_datos, "matriz varianza")
+
+#-------------------------------
+# Guardar la imagen FITS con los datos de la varianza
+#nombre_archivo_varianza = 'varianza_imagen.fits'
+#fits.writeto(nombre_archivo_varianza, varianza_acumulada, overwrite=True)
+
+# Guardar la imagen FITS con los datos del promedio
+#nombre_archivo_promedio = 'promedio_imagen.fits'
+#fits.writeto(nombre_archivo_promedio, promedio_datos, overwrite=True)
+
+#print(f"Se han creado las imágenes de promedio '{nombre_archivo_promedio}' y varianza '{nombre_archivo_varianza}'.")
+
+import matplotlib.pyplot as plt
+
+# Leer los datos del archivo FITS de promedio y varianza
+#promedio_datos = fits.getdata('promedio_imagen.fits')
+#varianza_datos = fits.getdata('varianza_imagen.fits')
+
+# Convertir los datos de la imagen de 2D a 1D para facilitar el trazado
+promedio_flat = promedio_datos.flatten()
+varianza_flat = varianza_datos.flatten()
+#------------
+umbral = 1000
+elementos_mayor_umbral = varianza_flat[varianza_flat > umbral]
+print(elementos_mayor_umbral)
+print(len(elementos_mayor_umbral))
+print(varianza_flat)
+print(len(varianza_flat))
+#----------------------------------
+# Graficar la varianza vs el promedio
+plt.figure(figsize=(8, 6))
+plt.scatter(promedio_flat, varianza_flat, s=1, alpha=0.5)
+plt.xlabel('Promedio de los datos')
+plt.ylabel('Varianza de los datos')
+plt.title('Varianza vs Promedio')
+plt.grid(True)
+plt.show()
