545522071fbeae2e4652a06a14508746aa90bedf
Georg <124269910+Georgsch98@users.noreply.github.com>
2024-04-09 13:46:19 -0300

    Update generador im sinteticas.py

--- Calibración de CCD's/generador im sinteticas.py
+++ Calibración de CCD's/generador im sinteticas.py
@@ -6,21 +6,27 @@ import random
 carpeta_salida = 'im_sintetizada'
 if not os.path.exists(carpeta_salida):
     os.makedirs(carpeta_salida)
-N=3040
-M=4056
+N=100
+M=100
 g=1
-t=2
-
+t=100
+contador=0
 for p in range (t):
+    contador+=1
+    print(contador)
     Matriz=np.zeros((N,M))
-
+    sigma=100
     for i in range(N):
         for j in range(M):
             mu=i+j*N
+            #mu=10
             k=np.random.poisson(mu)
             k_g=g*k
+            k_g=np.random.normal(k_g,sigma)
             Matriz[i][j]=k_g
            
     matriz_im = os.path.join(carpeta_salida, 'matriz_im_' + str(p) + '.fits')
     print(matriz_im)
     fits.writeto(matriz_im, Matriz, overwrite=True)
+
+    #print(Matriz)
